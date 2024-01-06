34f3930c27f1b3375c49beaf93ae105c2322a592
Pridewolf <agus_blackarrow_00@hotmail.com>
2024-01-06 18:03:44 -0300

    First commit

---
+++ Current/main.py
@@ -0,0 +1,42 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import streamlit as st 
+
+from PIL import Image
+
+import tensorflow as tf
+
+
+def main(): 
+    st.title('Cifar10 Web Classifier')
+    st.write('Suba cualquier imagen que creas que sea en una de las clases y comprueba si la perdicción es correcta')
+    
+    file = st.file_uploader('Porfavor subo la imagen', type=['jpg', 'png', 'webp'])
+    if file:
+        image = Image.open(file)
+        st.image(image, use_column_width=True)
+        resized_image = image.resize((32, 32))
+        img_array = np.array(resized_image) / 255
+        print(img_array)
+        img_array = img_array.reshape((1, 32, 32, 3))
+
+        model = tf.keras.models.load_model('cifar10_model.h5')
+
+        predictions = model.predict(img_array)
+        cifar10_classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'frog', 'horse', 'ship', 'truck', 'dog']
+
+        fig, ax = plt.subplots()
+        y_pos = np.arange(len(cifar10_classes))
+        ax.barh(y_pos, predictions[0], align='center')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(cifar10_classes)
+        ax.invert_yaxis()
+        ax.set_xlabel('Probability')
+        ax.set_title('CIFAR10 Predictions')
+        st.pyplot(fig=fig)
+    else : 
+        st.text('No has subido ninguna imagen')
+
+
+if __name__ == '__main__':
+    main()
