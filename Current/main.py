70491a5dea57b9e38a3a2e9f223b9d9a920be6af
Pridewolf <agus_blackarrow_00@hotmail.com>
2024-01-10 14:07:08 -0300

    Changes in the text

--- Current/main.py
+++ Current/main.py
@@ -9,7 +9,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 def main(): 
     st.title('Cifar10 Web Classifier')
-    st.write('Suba cualquier imagen que sea en una de las clases (airplane, automobile, bird, cat, deer, frog, horse, ship, truck, dog) y comprueba si la perdicción es correcta')
+    st.write('Suba cualquier imagen que pertenezca en una de las clases (airplane, automobile, bird, cat, deer, frog, horse, ship, truck, dog) y comprueba si la perdicción es correcta')
+"RGAPI-35eaad54-0bd1-454c-adcc-32a6cf5c96e7"
     
     file = st.file_uploader('Porfavor subo la imagen', type=['jpg', 'png', 'webp'])
     if file:
